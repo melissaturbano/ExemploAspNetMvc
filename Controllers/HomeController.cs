7f7231be86f925b32ca88f3d4a01b3b5ed26f297
Melissa Turbano <melissaturbano@gmail.com>
2022-08-16 17:52:13 -0300

    adicionando formulário

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,12 @@ using ExemploAspNetMvc.Models;
 
 namespace ExemploAspNetMvc.Controllers;
 
+public class UserRequest
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+}
+
 public class HomeController : Controller 
 {
     private readonly ILogger<HomeController> _logger;
@@ -28,10 +34,25 @@ public class HomeController : Controller
         return View();
     }
 
-    public string TesteQueryString([FromQuery] string q, string nome)
+    public string TesteQueryString([FromQuery] string q, string nome) //fromquery pega as infos da url
     {
         return $"Chegou aqui {q} para {nome}";
     }
+
+
+    // fromform pega as infos apenas de formularios
+    public string TesteFormData([FromForm] UserRequest userRequest, [FromHeader] string valor) 
+    {
+        return $"Nome: {userRequest.Nome}, Email: {userRequest.Email}, Valor: {valor}";
+    }
+
+    public IActionResult Formulario()
+    {
+        return View();
+    }
+
+
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
